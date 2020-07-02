80748249a2dc96dab255b5db12211741de5232d0
TristanDCP <tristandacostapinto@gmail.com>
2020-07-02 14:43:21 +0200

    setup mainarea

--- index.php
+++ index.php
@@ -2,48 +2,49 @@
 
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>TP Flux RSS</title>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/css/mdb.min.css">
-  <link rel="stylesheet" href="assets/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>TP Flux RSS</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/mdb.min.css">
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 
 <body>
-  <!-- =========================================== header -->
-  <header>
+    <!-- =========================================== header -->
+    <header>
 
-  </header>
-  <!-- =========================================== navbar -->
-  <nav>
+    </header>
+    <!-- =========================================== navbar -->
+    <nav>
 
-  </nav>
-  <!-- =========================================== main -->
-  <main>
+    </nav>
+    <!-- =========================================== main -->
+    <main>
+        <section class="mainarea justify-content-around">
+<div>SLT1</div>
+<div>SLT2</div>
+<div>SLT3 </div>
+    
+        </section>
+    </main>
+    <!-- =========================================== footer -->
+    <footer>
 
-    <p>Contenu here</p>
-
-  </main>
-  <!-- =========================================== footer -->
-  <footer>
-
-  </footer>
+    </footer>
 
 
-  <script type="text/javascript" src="assets/js/jquery.min.js"></script>
-  <script type="text/javascript" src="assets/js/popper.min.js"></script>
-  <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="assets/js/mdb.min.js"></script>
-  <script type="text/javascript" src="assets/script.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/popper.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/mdb.min.js"></script>
+    <script type="text/javascript" src="assets/script.js"></script>
 
 </body>
 
